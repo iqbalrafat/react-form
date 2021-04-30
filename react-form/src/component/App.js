9866640306158537d0d2a1973b8ece285bd38ea0
iqbalrafat <iqbalrafat@yahoo.com>
2021-04-30 01:07:23 -0600

    [react-form]--setup api to fetch data

--- react-form/src/component/App.js
+++ react-form/src/component/App.js
@@ -3,19 +3,22 @@ import React, {Component} from "react";
 import axios from 'axios';
 import SearchBar from "./search-bar.component";
 class App extends Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos',{
+  state={images:[]}
+ onSearchSubmit = async (term) => {
+  const response =  await axios.get('https://api.unsplash.com/search/photos',{
     params:{query:term},  
     headers:{ 
         Authorization: 'Client-ID JbFsBbTZ1dtyrUjQ22f8zU-Z-SsVKD7VOamCADLWvFU'
       }
-    })
+    });
+    this.setState({images:response.data.results});
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        found={this.state.images.length}
       </div>
     );
   }

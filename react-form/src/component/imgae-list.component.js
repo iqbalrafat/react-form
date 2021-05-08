e19678e07e9d929b73ae159ca0aab74c171c6057
iqbalrafat <iqbalrafat@yahoo.com>
2021-05-07 23:55:21 -0600

    [react-form]-- add Imge-Card Compnent

--- react-form/src/component/imgae-list.component.js
+++ react-form/src/component/imgae-list.component.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './image-list.css';
+import ImageCard from './image-card.component'
 const ImageList = (props) => {
   // const images = props.images.map((image) => {
   //   return <img key={image.id} alt={image.description} src={image.urls.regular} />;
   //the above method can be rewrite by refactoring as bgYellow
-  const images = props.images.map(({ id, description, urls }) => {
-    return <img key={id} alt={description} src={urls.regular} />;
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
   });
   return <div className="image-list" >{images}</div>;
 };
